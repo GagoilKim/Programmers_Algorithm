e2b28afe0a909af2e26005a3d9ec4ce7a0d7ecc4
Gagoilkim <dkim558@wisc.edu>
2020-05-24 23:26:04 -0500

    complete

---
+++ tower.py
@@ -0,0 +1,16 @@
+def solution(heights):
+    answer = []
+    for i in range(0, len(heights)):
+        tmp = heights.pop()
+        bef = len(answer)
+        for j in range(0, len(heights)):
+            ind = len(heights)-j-1
+            if heights[ind]> tmp:
+                answer.append(ind+1)
+                break;
+        if bef == len(answer):
+            answer.append(0)
+    last = []
+    for i in range(0, len(answer)):
+        last.append(answer.pop())
+    return last
