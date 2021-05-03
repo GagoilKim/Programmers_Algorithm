204ce1d451d77a35e145bc041c213affc450172a
Gagoil <cplkimdh@gmail.com>
2021-05-03 21:53:34 +0900

    modified

--- phoneNumList.py
+++ phoneNumList.py
@@ -1,16 +1,10 @@
-import re
 def solution(phone_book):
     answer = True
-    minlen = len(phone_book[0])
-    ind = 0
-    for i in range(1, len(phone_book)):
-        if len(phone_book[i]) < minlen:
-            minlen = len(phone_book[i])
-            ind = i
-    tmp = phone_book[ind]
-    phone_book[ind] = '000'
-    for j in range(len(phone_book)):
-        front = re.findall("." * minlen, phone_book[j])
-        if front[0] == tmp:
-            answer = False
+    array = phone_book
+    array.sort()
+    for i in range(len(array) - 1):
+        tmp = array[i]
+        if tmp in array[i + 1]:
+            answer =  False
+            break
     return answer
